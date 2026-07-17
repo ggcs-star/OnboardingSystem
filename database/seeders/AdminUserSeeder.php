8b6de9557ec15fccd2e85c1b70c57ae7abf570af
Navin1012 <nc13760@gmail.com>
2026-07-17 17:49:11 +0530

    login core done

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create admin role if it doesn't exist
+        $role = Role::firstOrCreate([
+            'name' => 'admin',
+            'guard_name' => 'web',
+        ]);
+
+        // Create admin user if it doesn't exist
+        $user = User::firstOrCreate(
+            [
+                'email' => 'nc13760@gmail.com',
+            ],
+            [
+                'name' => 'Navin Chaudhary',
+                'password' => Hash::make('12345678'),
+            ]
+        );
+
+        // Assign role
+        $user->assignRole($role);
+    }
+}
--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -4,14 +4,48 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 class RolePermissionSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        $permissions = [
+            'users.view',
+            'users.create',
+            'users.edit',
+            'users.delete',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create([
+                'name' => $permission
+            ]);
+        }
+
+        $admin = Role::create([
+            'name' => 'admin'
+        ]);
+
+        $manager = Role::create([
+            'name' => 'manager'
+        ]);
+
+        $employee = Role::create([
+            'name' => 'employee'
+        ]);
+
+        $admin->givePermissionTo(Permission::all());
+
+        $manager->givePermissionTo([
+            'users.view'
+        ]);
+
+        $employee->givePermissionTo([
+            'users.view'
+        ]);
     }
 }
